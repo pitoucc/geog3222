1b521c66a5dd187a35432d07b8b53a37ec2a2edb
pitoucc <pitoucc@gmail.com>
2019-02-14 14:26:43 -0330

    added comments

---
+++ Lab_6/ChiassonK_201255916_Lab6.R
@@ -0,0 +1,37 @@
+# Geog 3222 - Lab 6
+# Karl Chiasson
+# 201255916
+
+#Load data/scripts
+
+#Applies grouping.ts to get daily maxima
+
+#Declusters wind data to identify "wind storms"
+
+#Prints a paragraph explaining the output from decluster
+
+#Applies fitdistr to identify the parameters of best fitting Poison and NB distributions
+
+#Prints the parameters and the statistics of annual count data.
+
+#Plots a histogram of annual windstorm counts
+
+#Uses parameters to add lines to show poisson and nb distribution
+
+#Calculates the probability of anniual windstorm counts using poisson and nb distributions
+
+#Prints a paragraph discussing the suitability of the distributions for the data
+
+#Identifies the parameters of best fitting NB and geometric distributions of windstorm duration data
+
+#Prints the parameters
+
+#Plots a histogram of windstorm durations
+
+#Uses the parameters to add lines to the histrogram for geometric and nb distributions
+
+#Calculates the probability of the windstorm counts using geometric and NB distributions
+
+#Prints a paragraph discussing the suitability of the distributions for the data
+
+#Prints a paragraph discussing the probabilities
