820a0030f445d49f151cd9b17db4454c129cd436
pitoucc <pitoucc@gmail.com>
2019-02-28 14:31:01 -0330

    added comments

---
+++ Lab_7/ChiassonK_201255916_Lab7.R
@@ -0,0 +1,23 @@
+#Geog 3222 Lab 7
+#Karl Chiasson
+#201255916
+
+#Load required scripts/data/libraries
+
+#Prints a paragraph about which data set will fit to a normal distribution
+
+#Extracts data into sets
+
+#Makes a qq.plot for each data set
+
+#Prints a paragraph comparing the qq.plots
+
+#Fits Gaussian and Weibull to each wind vector
+
+#Reports the likelyhood of each fit
+
+#Performs ks.test to each distribution and reports the p value of each.
+
+#Prints a paragraph comparing ks.test results
+
+#Prints paragraph summerizing results
