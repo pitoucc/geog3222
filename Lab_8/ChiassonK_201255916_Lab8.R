7d56036ac668f4f3078cf41f4290ac523af0eb97
pitoucc <pitoucc@gmail.com>
2019-03-07 14:26:51 -0330

    added comments

--- Lab_8/ChiassonK_201255916_Lab8.R
+++ Lab_8/ChiassonK_201255916_Lab8.R
@@ -6,3 +6,45 @@
 setwd("D:\\Class\\geog_3222\\Labs\\Lab_8")
 
 #Load scripts/libraries/data
+library(evd)
+load("Dataset3.Rdata")
+source("decluster.g2222.R")
+source("event.decluster.R")
+source("grouping.ts.R")
+source("pareto.thresh.decluster.R")
+
+#Gets annual maxima
+
+#Gets max windspeeds for wind events
+
+#Estimates the number of high wind events per year
+
+#Fits GEV distribution to block maxima data
+
+#Estimates 100 year event
+
+#Fits POT distribution to event peak data
+
+#Plots hist 
+
+#Plots hist
+
+#Prints paragraph about the quality of parametric fits
+
+#Calculates event probability
+
+#Estimates 100 year event based on POT approach
+
+#Prints a paragraph comparing 100 year estimates
+
+#Calculates prob of meet/exceeding Cat 1 Winds
+
+#Coverts prob to return period
+
+#Uses GP dist to calculate prob that wind event meet/exceeding Cat 1 Winds
+
+#Calculates the number of events needed to observe hurricane force winds
+
+#Estimates annual frequency and return period of hurricane winds
+
+#Prints paragraph accessing hurricane risks
